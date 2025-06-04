f987c04f729e6f6dfd7e2fe70aed15a0a253adad
Tomislav Dukez <tomdu3@ymail.com>
2025-06-04 11:59:12 +0100

    update view with django api_view

--- api/views.py
+++ api/views.py
@@ -1,13 +1,11 @@
-from django.http import JsonResponse
 from api.serializers import ProductSerializer
 from api.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
-    return JsonResponse(
-        {
-            'data': serializer.data
-        }
-    )
+    return Response(serializer.data)
 
