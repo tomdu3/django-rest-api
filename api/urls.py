7af188cc20ff78be149c57c2a155978d832eb8c8
Tomislav Dukez <tomdu3@ymail.com>
2025-06-04 12:51:13 +0100

    add orders and order details views

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', views.product_list, name='product_list'),
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('orders/', views.order_list, name='order_list'),
+    path('orders/<uuid:order_id>/', views.order_detail, name='order_detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,15 @@
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import (
+    ProductSerializer,
+    OrderSerializer,
+    OrderItemSerializer
+)
+from api.models import (
+    Product,
+    Order,
+    OrderItem,
+)
+
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
@@ -15,4 +25,16 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_detail(request, order_id):
+    order = get_object_or_404(Order, order_id=order_id)
+    serializer = OrderSerializer(order)
     return Response(serializer.data)
