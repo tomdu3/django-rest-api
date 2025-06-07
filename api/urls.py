56df1c0c8cbf182a6e33dd7412a1d886e39f0751
Tomislav Dukez <tomdu3@ymail.com>
2025-06-07 02:08:40 +0100

    refactor pep8 compliance

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,17 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductListAPIView.as_view(), name='product_list'),
-    path('products/<int:pk>/', views.ProductDetailAPIView.as_view(), name='product_detail'),
+    path(
+        'products/<int:pk>/',
+        views.ProductDetailAPIView.as_view(),
+        name='product_detail'
+        ),
     path('products/info/', views.product_info, name='product_info'),
     path('orders/', views.OrderListAPIView.as_view(), name='order_list'),
     path('orders/<uuid:order_id>/', views.order_detail, name='order_detail'),
-    path('orders/user/', views.UserOrderListAPIView.as_view(), name='user_order_list'),
+    path(
+        'orders/user/',
+        views.UserOrderListAPIView.as_view(),
+        name='user_order_list'
+        ),
 ]
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from django-rest-api!")
-
-
-if __name__ == "__main__":
-    main()
