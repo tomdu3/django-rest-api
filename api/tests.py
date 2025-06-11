be461120946fdbf70fd3b56e7bd631cc95e1a0dd
Tomislav Dukez <tomdu3@ymail.com>
2025-06-11 22:11:01 +0100

    add test for authenticated user orders endpoint

--- api/tests.py
+++ api/tests.py
@@ -1,1 +1,31 @@
 from django.test import TestCase
+from api.models import User, Order
+from rest_framework.reverse import reverse
+
+class UserOrderListAPITestCase(TestCase):
+    def setUp(self):
+        user1 = User.objects.create_user(
+            username='user1',
+            password='password1',
+            email='user1@example.com'
+        )
+        user2 = User.objects.create_user(
+            username='user2',
+            password='password2',
+            email='user2@example.com'
+        )
+        Order.objects.create(user=user1)
+        Order.objects.create(user=user1)
+        Order.objects.create(user=user2)
+        Order.objects.create(user=user2)
+        
+    def test_user_order_retrieves_only_authenticated_user_orders(self):
+        # login as user1
+        user = User.objects.get(username='user1')
+        self.client.force_login(user)
+        response = self.client.get(reverse('user_order_list'))
+        
+        assert response.status_code == 200
+        data = response.json()
+        print(data)
+        assert len(data) == 2
