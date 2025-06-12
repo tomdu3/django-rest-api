4ec14288b8e4f6837ef60896ba82c1534a168dc6
Tomislav Dukez <tomdu3@ymail.com>
2025-06-12 22:11:10 +0100

    fix remove redundant imports

--- api/views.py
+++ api/views.py
@@ -12,8 +12,6 @@ from api.models import (
 
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
